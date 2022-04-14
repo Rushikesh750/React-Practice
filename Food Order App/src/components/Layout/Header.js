167f6321c1e224afded1bc0ea0934a531282b3e4
Rushikesh More <46043053+Rushikesh750@users.noreply.github.com>
2022-04-14 16:15:56 +0530

    added preetier configuration (#19)
    Co-authored-by: IN_MOURITECH\rushikeshm <rushikeshm.in@mouritech.com>

--- Food Order App/src/components/Layout/Header.js
+++ Food Order App/src/components/Layout/Header.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import mealsImage from "../../assets/meals.jpg";
-import classes from "./Header.module.css";
-import HeaderCartButton from "./HeaderCarButton";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCarButton';
 
 const Header = (props) => {
   return (
@@ -11,7 +11,7 @@ const Header = (props) => {
         <h1>ReactMeals</h1>
         <HeaderCartButton />
       </header>
-      <div className={classes["main-image"]}>
+      <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </>
