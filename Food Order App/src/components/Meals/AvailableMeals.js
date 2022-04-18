476efbb53e06640549ef234d6195238c3067166a
IN_MOURITECH\rushikeshm <rushikeshm.in@mouritech.com>
2022-04-18 17:23:29 +0530

    added form

--- Food Order App/src/components/Meals/AvailableMeals.js
+++ Food Order App/src/components/Meals/AvailableMeals.js
@@ -32,10 +32,11 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
+      id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
-      price={meal.price}
+      price={meal.price }
     />
   ));
 
